c3e14fafa5c4176c88bf61a48129d511d88a0c73
raquel2548 <rehg1234@gmail.com>
2022-10-17 15:26:35 -0700

    extensions work

---
+++ RaquelVRRoom/Assets/FunctioningClock.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FunctioningClock : MonoBehaviour
+{
+    public GameObject hourhand;
+    public GameObject minutehand;
+    public GameObject secondhand;
+
+//    public int Second { get; }
+//    DateTime localDate = DateTime.Now;
+   
+//    public static DateTime.Now;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+//        string second = DateTime.now;
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
