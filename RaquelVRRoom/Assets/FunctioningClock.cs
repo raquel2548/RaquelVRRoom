53d8d8e0572e771d6b6bc50e882e9746eb179733
raquel2548 <rehg1234@gmail.com>
2022-11-15 15:28:20 -0800

    finished arrangable art and started clock

--- RaquelVRRoom/Assets/FunctioningClock.cs
+++ RaquelVRRoom/Assets/FunctioningClock.cs
@@ -8,21 +8,30 @@ public class FunctioningClock : MonoBehaviour
     public GameObject hourhand;
     public GameObject minutehand;
     public GameObject secondhand;
+    string oldSeconds;
 
-//    public int Second { get; }
-//    DateTime localDate = DateTime.Now;
-   
-//    public static DateTime.Now;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-//        string second = DateTime.now;
-    }
- 
-    // Update is called once per frame
     void Update()
     {
+        string seconds = System.DateTime.UtcNow.ToString("ss");
         
+
+        if (seconds != oldSeconds)
+        {
+            UpdateTimer();
+        }
+        oldSeconds = seconds;
+    }
+
+    void UpdateTimer()
+    {
+        int secondsInt = int.Parse(System.DateTime.UtcNow.ToString("ss"));
+        int minutesInt = int.Parse(System.DateTime.UtcNow.ToString("mm"));
+        int hoursInt = int.Parse(System.DateTime.UtcNow.ToLocalTime().ToString("hh"));
+        print(hoursInt + " : " + minutesInt + " : " + secondsInt);
+
+       // Rotate(secondhand, ("z", secondsInt * 6 * -1));
+       // Rotate(minutehand, ("z", minutesInt * 6 * -1));
+       // float hourDistance = (float)(minutesInt) / 60f;
+       // Rotate(hourhand, ("z", (hoursInt + hourDistance) * 360 / 12 * -1));
     }
 }
