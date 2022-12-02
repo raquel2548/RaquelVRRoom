cfb5710b554c5bc023895fe53429072ffd4d4ea3
raquel2548 <rehg1234@gmail.com>
2022-12-02 15:28:20 -0800

    clock

--- RaquelVRRoom/Assets/FunctioningClock.cs
+++ RaquelVRRoom/Assets/FunctioningClock.cs
@@ -7,10 +7,10 @@ using UnityEngine;
 
 public class FunctioningClock : MonoBehaviour
 {
-    public GameObject hourHand;
-    public GameObject minuteHand;
+    
     public GameObject secondHand;
-    //string oldSeconds;
+    public GameObject minuteHand;
+    public GameObject hourHand;
 
     void Update()
     {
@@ -19,25 +19,8 @@ public class FunctioningClock : MonoBehaviour
         secondHand.transform.localRotation = Quaternion.Euler(new Vector3(secondDegree, 0, 0));
         float minuteDegree = (currentTime.Minute / 60f) * 360f;
         minuteHand.transform.localRotation = Quaternion.Euler(new Vector3(minuteDegree, 0, 0));
-        float hourDegree = (currentTime.Hour / 60f) * 360f;
+        float hourDegree = (currentTime.Hour / 12f) * 360f;
         hourHand.transform.localRotation = Quaternion.Euler(new Vector3(hourDegree, 0, 0));
-        //string seconds = System.DateTime.UtcNow.ToString("ss");
-
-
-
     }
 
-  //  void UpdateTimer()
-    
-        //int secondsInt = int.Parse(System.DateTime.UtcNow.ToString("ss"));
-        //int minutesInt = int.Parse(System.DateTime.UtcNow.ToString("mm"));
-        //int hoursInt = int.Parse(System.DateTime.UtcNow.ToLocalTime().ToString("hh"));
-        //print(hoursInt + " : " + minutesInt + " : " + secondsInt);
-
-   
-    //if (seconds != oldSeconds)
-        
-            //UpdateTimer();
-        
-        //oldSeconds = seconds;
 }
